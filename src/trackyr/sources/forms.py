c81f3022641d896f52b252c069e9f5b2473eea71
Fyre <fyre.homelab+gitkraken@gmail.com>
2020-06-20 13:11:47 -0500

    update zwillow

--- src/trackyr/sources/forms.py
+++ src/trackyr/sources/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
 
 class SourceForm(FlaskForm):
-    MODULE_CHOICES = [(0,'Please Select a Module'), (1,'Kijiji')]
+    MODULE_CHOICES = [(0,'Please Select a Module'), (1,'Kijiji'), (2, 'Zwillow')]
     module = SelectField('Module', validators=[DataRequired()], coerce=int, choices=MODULE_CHOICES)
     name = StringField('Name', validators=[DataRequired()])
     website = StringField('Website')
